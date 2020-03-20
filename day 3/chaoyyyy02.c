13f1431160118068bb9aa7983bf507d01cab715e
chaoyyyy1111 <774881706@qq.com>
2020-03-20 16:03:07 +0800

    提交代码

---
+++ day 3/chaoyyyy02.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	struct Student
+//	{
+//		char name[10];
+//		int age;
+//		int id;
+//
+//	};
+//	struct Student Lucy = { "郑秀",22,62013232 };
+//	struct Student* ps = &Lucy;
+//	printf("姓名：%s\n", Lucy.name);
+//	printf("年龄：%d\n", Lucy.age);
+//	printf("ID：%d\n", Lucy.id);
+//
+//	printf("姓名：%s\n", (*ps).name);
+//	printf("年龄：%d\n", ps->age);
+//	printf("ID：%d\n", ps->id);
+//	return 0;
+//}
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一");
+//		break;
+//	case 2:
+//		printf("星期二");
+//		break;
+//	case 3:
+//		printf("星期三");
+//		break;
+//	case 4:
+//		printf("星期四");
+//		break;
+//	case 5:
+//		printf("星期五");
+//		break;
+//	case 6:
+//		printf("星期六");
+//		break;
+//	case 7:
+//		printf("星期天");
+//		break;
+//	default:
+//		printf("输入错误");
+//		break;
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	
+//while (i<10)
+//{
+//	if (i == 5)
+//		break;
+//
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+int main()
+{
+	char arr[] = "bit";
+	char arr1[] = { 'b', 'i', 't' };
+	printf("%s\n", arr);
+	printf("%s\n", arr1);
+	printf("%d\n", strlen(arr));
+	printf("%d\n", strlen(arr1));
+	return 0;
+}
