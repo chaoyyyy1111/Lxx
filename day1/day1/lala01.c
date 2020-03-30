e63bba3d66d1e51fd66f44062067479ae31d7e8a
chaoyyyy1111 <774881706@qq.com>
2020-03-30 16:25:54 +0800

    提交代码

---
+++ day1/day1/lala01.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#define MAX(a,b) ((a)>(b)?(a):(b))
+//#include <stdio.h>;
+//#include <string.h>;
+//int max(int a, int b)
+//{
+//	return a > b ? a: b;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 4;
+//	int sum1 = max(a,b);
+//	int sum2 = MAX(a, b);
+//	printf("sum1=%d", sum1);
+//	printf("sum2=%d", sum2);
+//
+//}
+//int main()
+//{
+//	enum color
+//	{
+//		RED,
+//		YELLOE,
+//		BLUE
+//	};
+//	enum color beauty = RED;
+//	printf("%d", beauty);
+//} 
+//int main()
+//{
+//	int input = 0;
+//	while (input != 1)
+//	{
+//	printf("加入比特，你要好好学习吗?\n");
+//	printf("好好学习输入1，否则输入0\n");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("可以找到好工作\n");
+//	}
+//	else
+//	{
+//		printf("回家卖红薯\n");
+//	}
+// }
+//}
+//int main()
+//{
+//	int arr[10] = {0};
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sz);
+//	while (i < 10)
+//	{
+//		arr[i] = i;
+//		i++;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//
+//	}
+//}
+//int main()
+//{
+//	int line = 0;
+//	while (line < 20000)
+//	{
+//		printf("line=%d", line);
+//		printf("继续打代码\n");
+//		line++;
+//	}
+//	printf("找到好工作\n");
+//}
+//int main()
+//{
+//	typedef unsigned int un_int;
+//	un_int Lucy = 18;
+//	int a=sizeof(long);
+//	printf("%d,%d", a,Lucy);
+//}
+//void test()
+//{
+//	static int a = 0;
+//	printf("a=%d\n", a);
+//	a++;
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10){
+//		test();
+//		i++;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	int* p = &a;
+//	*p = 20;
+//	printf("%p\n", p);
+//	printf("%d\n", *p);
+//	return 0;
+//}
+
+
+
+
+
