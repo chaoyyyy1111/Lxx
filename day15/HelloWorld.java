3ba900f1e9d208b8bbbe922fe49665d3e0937a45
chaoyyyy1111 <774881706@qq.com>
2020-04-07 13:27:56 +0800

    提交代码

---
+++ day15/HelloWorld.java
@@ -0,0 +1,31 @@
+/**
+文件名：HelloWorld
+作者：Lxx
+日期：20200407
+描述：
+    如果一个类是public的 那么这个类的类名  一定要和文件名相同，并且一个java文件当中
+只能有一个共有的类。  
+*/
+class A {
+	
+}
+public class HelloWorld {
+	public static void main(String[]args) {
+		int a=0;
+		System.out.println(a);
+		System.out.println("HelloWorld");
+		int min=Integer.MAX_VALUE+1;
+		int max=Integer.MIN_VALUE-1;
+		System.out.println(min);
+		System.out.println(max);
+		System.out.println(Integer.MAX_VALUE+2);
+	}
+	
+} 
+
+
+
+
+
+
+
